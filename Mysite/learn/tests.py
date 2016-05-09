71621d6b458b47105e83d03147dd95590c2af2d2
maoskito <puchangyu01@163.com>
2016-05-09 18:12:57 +0800

    更新

--- Mysite/learn/tests.py
+++ Mysite/learn/tests.py
@@ -10,26 +10,26 @@ from django.template import Template
 from django.template import Context
 
 ##t = Template('{% notatag %}')
-raw_template = """<p>Dear {{ person_name.1 }},</p>
-...
-... <p>Thanks for placing an order from {{ company }}. It's scheduled to
-... ship on {{ ship_date|date:"F j, Y" }}.</p>
-...
-... {% if ordered_warranty %}
-... <p>Your warranty information will be included in the packaging.</p>
-... {% else %}
-... <p>You didn't order a warranty, so you're on your own when
-... the products inevitably stop working.</p>
-... {% endif %}
-...
-... <p>Sincerely,<br />{{ company }}</p>"""
-t = Template(raw_template)
-import datetime
-c = Context({'person_name': 'John Smith',
-             'company': 'Outdoor Equipment',
-             'ship_date': datetime.date(2009, 4, 2),
-             'ordered_warranty': False})
-c_x = Context( {'person_name': ['Markov','Bano','Maoskito']
-    })
-print t.render(c)
-print t.render(c_x)
+class SilentAssertionError(AssertionError):
+    silent_variable_failure = True
+
+class PersonClass3(object):
+    
+    def first_name(self):
+        raise SilentAssertionError, "foo"
+    def delete(self):
+        print 'delete!'
+    delete.alters_data = True
+
+t = Template('''
+{% for athlete in athlete_list %}
+    <p>{{ athlete }}</p>
+    {{forloop.first}}
+{% empty %}
+    <p>There are no athletes. Only computer programmers.</p>
+{% endfor %}
+''')
+
+
+c = { 'athlete_list':['','test_1','test_2','test_3']}
+print t.render(Context(c))
