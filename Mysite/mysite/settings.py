2d280f9155392a42af3cb8de66ed0ec7d032ebeb
maoskito <puchangyu01@163.com>
2016-06-12 18:14:24 +0800

    更新

--- Mysite/mysite/settings.py
+++ Mysite/mysite/settings.py
@@ -77,11 +77,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
+##    'default': {
+##        'ENGINE': 'django.db.backends.sqlite3',
+##        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+##    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '192.168.172.7',
+            'NAME': 'PCY_TEST_01',
+            'PASSWORD': 'rwy123',
+            'PORT': '3306',
+            'USER': 'rwy'
+        }
 }
 
 
